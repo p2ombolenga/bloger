0e35fce1d807efc7aef008ea2799b6984027b42c
p2ombolenga <nsabimanapeter2000@gmail.com>
2026-07-15 03:37:31 +0200

    Register comment model in admin site and implement syntax best practices

--- publisher/admin.py
+++ publisher/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'body', 'user', 'created_at')
-    search_fields = ('title', 'body')
-    list_display_links = ('title', 'body')
-    list_filter = ('created_at', 'user')
+    list_display = ['id', 'title', 'body', 'user', 'created_at']
+    search_fields = ['title', 'body']
+    list_display_links = ['title', 'body']
+    list_filter = ['created_at', 'user']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'content', 'user', 'post__title']
+    search_fields = ['content', 'user__username']
+    list_display_links = ['content']
+    list_filter = ['content', 'user', 'post']

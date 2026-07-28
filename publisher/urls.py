fd5effed6b347980730aaf2504f623590a46f7d2
p2ombolenga <nsabimanapeter2000@gmail.com>
2026-07-29 00:18:13 +0200

    Implement sign in with google using google-auth helper

--- publisher/urls.py
+++ publisher/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('api/auth/google/', views.GoogleLoginAPIView.as_view(), name='google-login'),
     path('posts/', views.PostListCreateAPIView.as_view(), name='post-list'),
     path('posts/<int:pk>/', views.PostRetrieveUpdateDestroyAPIView.as_view(), name='post-detail'),
     path('posts/<int:post_id>/comments/', views.CommentListCreateAPIView.as_view(), name='comment-list'),
--- publisher/views.py
+++ publisher/views.py
@@ -1,9 +1,53 @@
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
+from google.oauth2 import id_token
+from google.auth.transport import requests
+from decouple import config
+
+
+class GoogleLoginAPIView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        token = request.data.get('token')
+
+        try:
+            idinfo = id_token.verify_oauth2_token(token, requests.Request(), config('GOOGLE_CLIENT_ID'))
+            email = idinfo['email']
+            first_name = idinfo.get('given_name', '')
+            last_name = idinfo.get('family_name', '')
+
+            user, created = User.objects.get_or_create(
+                email=email,
+                defaults={
+                    'username': email.split('@')[0],
+                    'first_name': first_name,
+                    'last_name': last_name
+                }
+            )
+
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'email': user.email,
+                'is_new_user': created
+            })
+
+        except ValueError:
+            return Response(
+                {'error': 'Invlid Google Token'},
+                status=status.HTTP_400_BAD_REQUEST
+                )
 
 class PostListCreateAPIView(ListCreateAPIView):
     queryset = Post.objects.all().prefetch_related('comments')
